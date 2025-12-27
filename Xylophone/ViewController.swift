7fbafa1ed35ffa82069f34cf0de12053ccb8db37
Zane Clark <zanebclark@gmail.com>
2025-12-27 10:16:48 -0800

    feat: dim button when pressed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -52,7 +52,14 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
         let keyLabel = sender.titleLabel?.text
         SoundPlayer.shared.playSound(named: keyLabel!)
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
     }
 }
